98318c3ec6e5178e051006b52859afbf4860f6f9
muneerah <muneerah.aldhasy@gmail.com>
2024-10-20 16:47:13 +0300

    Initial Commit

---
+++ Plants/PlantsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlantsApp.swift
+//  Plants
+//
+//  Created by muneerah aldhasi on 17/04/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct PlantsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PlantsTests/PlantsTests.swift
@@ -0,0 +1,17 @@
+//
+//  PlantsTests.swift
+//  PlantsTests
+//
+//  Created by muneerah aldhasi on 17/04/1446 AH.
+//
+
+import Testing
+@testable import Plants
+
+struct PlantsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
